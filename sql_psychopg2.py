b1ed4940f95c9a46b731601f23233040c5a2218f
Martina Grganic <totalnobzvz@yahoo.com>
2022-01-26 00:14:22 +0000

    Read data from chinook database using python module psychopg2

---
+++ sql_psychopg2.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+# Connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# Build a cursor object ofthe database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track"
+# table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Fetch the results (multiple)
+results = cursor.fetchall()
+
+# Fetch the result (single)
+# results = cursor.fetchone()
+
+# Close the connection
+connection.close()
+
+for result in results:
+    print(result)
